418e3e5988bb247b0cbf18ad3c9d51f5e2839cfb
KoToZ <mohamedtareck95@gmail.com>
2016-08-14 01:32:03 +0200

    Add files via upload

---
+++ push_button.ino
@@ -0,0 +1,20 @@
+// Push button 
+
+const int Button = 2 ; 
+const int LED = 3 ; 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Button,INPUT);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(Button)== HIGH){
+      digitalWrite(LED,HIGH);
+    }
+    else{
+        digitalWrite(LED,LOW);
+      }
+
+}
